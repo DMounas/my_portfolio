33d8449573abfb24f5000d92d7a7007bb58c35bd
DMounas <debangshumounas.dev@gmail.com>
2025-06-22 00:39:53 +0530

    Update build.sh
    Update Flutter to 3.32.4

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # --- Set a specific, stable Flutter version ---
-FLUTTER_VERSION="3.22.2"
+FLUTTER_VERSION="3.32.4"
 
 # --- Install Flutter and dependencies ---
 echo "Cloning Flutter SDK version $FLUTTER_VERSION..."
